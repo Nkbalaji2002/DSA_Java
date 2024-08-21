8e872f7e88b7ccbdbfecca8c466dfef8be09c4b2
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2024-08-21 19:13:48 +0530

    Search the key in Binary Search Tree

--- src/tree/bst/BinarySearchTree.java
+++ src/tree/bst/BinarySearchTree.java
@@ -55,16 +55,40 @@ public class BinarySearchTree {
         inorder(root);
     }
 
+    // Search the key in bst
+    public TreeNode search(int key) {
+        return search(root, key);
+    }
+
+    public TreeNode search(TreeNode root, int key) {
+        if (root == null || root.data == key) {
+            return root;
+        }
+
+        if (key < root.data) {
+            return search(root.left, key);
+        } else {
+            return search(root.right, key);
+        }
+    }
+
     public static void main(String[] args) {
         BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(6);
+        bst.insert(4);
+        bst.insert(8);
+        bst.insert(2);
         bst.insert(5);
-        bst.insert(3);
         bst.insert(7);
-        bst.insert(1);
+        bst.insert(9);
 
-        bst.inorder();
+        // bst.inorder();
+        if (null != bst.search(5)) {
+            System.out.println("Key Found!!");
+        } else {
+            System.out.println("Key not Found!");
+        }
 
-        System.out.println();
     }
 
 }

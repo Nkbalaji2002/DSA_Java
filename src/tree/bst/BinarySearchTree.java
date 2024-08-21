3957f2245d0a69fbdadd2bd98e8bf3520c5ecb3d
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2024-08-21 09:14:49 +0530

    Insert the value and inorder method commit

--- src/tree/bst/BinarySearchTree.java
+++ src/tree/bst/BinarySearchTree.java
@@ -19,4 +19,52 @@ public class BinarySearchTree {
 
     }
 
+    // insert the value using root
+    public void insert(int value) {
+        root = insert(root, value);
+    }
+
+    public TreeNode insert(TreeNode root, int value) {
+        // base case
+        if (root == null) {
+            root = new TreeNode(value);
+            return root;
+        }
+
+        if (value < root.data) {
+            root.left = insert(root.left, value);
+        } else {
+            root.right = insert(root.right, value);
+        }
+
+        return root;
+    }
+
+    // inorder mehtod in bst
+    public void inorder(TreeNode root) {
+        if (root == null) {
+            return;
+        }
+
+        inorder(root.left);
+        System.out.print(root.data + " ");
+        inorder(root.right);
+    }
+
+    public void inorder() {
+        inorder(root);
+    }
+
+    public static void main(String[] args) {
+        BinarySearchTree bst = new BinarySearchTree();
+        bst.insert(5);
+        bst.insert(3);
+        bst.insert(7);
+        bst.insert(1);
+
+        bst.inorder();
+
+        System.out.println();
+    }
+
 }

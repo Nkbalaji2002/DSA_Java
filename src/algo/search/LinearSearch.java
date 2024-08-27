10dfe57af73ff0a51768946a9e8ac360e4ea30c7
Nkbalaji2002 <nithishkumarbsc@gmail.com>
2024-08-27 07:39:29 +0530

    Linear Search Changes

--- src/algo/search/LinearSearch.java
+++ src/algo/search/LinearSearch.java
@@ -2,4 +2,24 @@ package algo.search;
 
 public class LinearSearch {
 
+    public static void main(String[] args) {
+        LinearSearch obj = new LinearSearch();
+
+        int[] arr = { 5, 1, 9, 2, 10, 15, 20 };
+
+        int result = obj.search(arr, 10);
+        System.out.println("result : " + result);
+    }
+
+    public int search(int[] arr, int x) {
+
+        for (int i = 0; i < arr.length; i++) {
+            if (arr[i] == x) {
+                return i;
+            }
+        }
+
+        return -1;
+    }
+
 }
